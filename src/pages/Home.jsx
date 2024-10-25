c30445ecf4718308fa6959401fd8cdc89487bcf8
dlgotjd1123 <dlgotjd1123@naver.com>
2024-10-25 14:37:54 +0900

    메인페이지디자인

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,8 +2,29 @@ import Layout from '../components/Layout';
 
 export default function Home() {
 	return (
-		<Layout title='Home'>
-			<p>Home Page</p>
-		</Layout>
-	);
-}
+		<div className="main-page">
+			<div className="main-content">
+				<div className="main-image">
+					<Layout title='Home'>		
+					<p>Lorem ipsum dolor sit amet.</p>				
+					</Layout>
+				</div>
+				<div className="grid-section">
+					<div className="grid-item">
+						<img src="/src/pages/img/img1.jpg" alt=" 1" />
+						<p>Activities</p>
+					</div>
+					<div className="grid-item">
+						<img src="/src/pages/img/img2.jpg" alt=" 2" />
+						<p>Gallery</p>
+					</div>
+					<div className="grid-item">
+						<img src="/src/pages/img/img3.jpg" alt=" 3" />
+						<p>Program</p>
+					</div>
+				</div>
+			</div>
+		</div>
+		);
+	}
+	
