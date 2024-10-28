df84b3dc2547e3d0ed5b605fb244fa153836fc49
dlgotjd1123 <dlgotjd1123@naver.com>
2024-10-28 14:24:14 +0900

    hotel페이지

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,13 @@
-//모든 페이지 컴포넌트가 해당 컴포넌트를 호출해서 공통의 jsx구조를 공유
-//props를 통해 각 페이지 컴포넌트로부터 제목텍스트와 컨텐츠 내용을 전달 받음
 export default function Layout(props) {
+	// props.title이 정의되어 있는지 확인하여 처리
+	const title = props.title ? props.title.toLowerCase() : 'default-title';
+
 	console.log(props); //{title: '페이지제목', children: 모든 자식 jsx요소 }
+
 	return (
 		// 클래스명에 등록할때는 무조건 소문자로 변형해서 등록
-		<main className={props.title.toLowerCase()}>
-			<h2>{props.title}</h2>
+		<main className={title}>
+			<h2>{props.title || 'Default Title'}</h2> {/* title이 없으면 기본값 표시 */}
 			{props.children}
 		</main>
 	);
--- src/pages/Hotel.jsx
+++ src/pages/Hotel.jsx
@@ -1,9 +1,98 @@
-import Layout from '../components/Layout';
+import { useState, useEffect } from 'react';
+import Layout from '../components/Layout'; 
 
-export default function Hotel() {
-	return (
-		<Layout title='Hotel'>
-			<p>Hotel Page</p>
-		</Layout>
-	);
+const roomData = [
+{
+    type: "싱글룸",
+    images: [
+		"/src/pages/img/single1.jpg",
+		"/src/pages/img/single2.jpg",
+		"/src/pages/img/single3.jpg",
+		"/src/pages/img/single4.jpg",
+		"/src/pages/img/single5.jpg"
+    ],
+    description: "싱글룸 설명",
+    subTitle: "Vista Walkerhill Seoul - Grand Walkerhill Seoul",
+    info: "Human, Nature, and Technology"
+},
+{
+    type: "커플룸",
+    images: [
+		"/src/pages/img/couple1.jpg",
+		"/src/pages/img/couple2.jpg",
+		"/src/pages/img/couple3.jpg",
+		"/src/pages/img/couple4.jpg",
+		"/src/pages/img/couple5.jpg"
+    ],
+    description: "커플룸 설명",
+    subTitle: "Douglas House",
+    info: "Romantic Getaway for Couples"
+},
+{
+    type: "단체룸",
+    images: [
+		"/src/pages/img/group1.jpg",
+		"/src/pages/img/group2.jpg",
+		"/src/pages/img/group3.jpg",
+		"/src/pages/img/group4.jpg",
+		"/src/pages/img/group5.jpg"
+    ],
+    description: "단체룸 설명",
+    subTitle: "Group-friendly Space",
+    info: "Perfect for Group Gatherings"
 }
+];
+
+const RoomSlider = ({ room }) => {
+	const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+		setCurrentImageIndex((prevIndex) => (prevIndex + 1) % room.images.length);
+		}, 3000); // 3초마다 이미지 변경
+
+		return () => clearInterval(interval); // 컴포넌트가 언마운트될 때 타이머 정리
+	}, [room.images.length]);
+
+	return (
+		<div className="room">
+			<div className="room-info">
+				<h2>{room.type}</h2>
+				<p>{room.subTitle}</p>
+				<p>{room.info}</p>
+				<p>{room.description}</p>
+			</div>
+			<div className="room-slider">
+				<div
+				className="slider-images"
+				style={{
+				transform: `translateX(-${currentImageIndex * 100}%)`,
+				}}
+				>
+				{room.images.map((image, index) => (
+					<img
+					key={index}
+					src={image}
+					alt={room.type}
+					className="room-image"
+					/>
+				))}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+const Hotel = () => {
+	return (
+    <Layout title="객실소개">
+		<div className="hotel-container">
+			{roomData.map((room, index) => (
+			<RoomSlider key={index} room={room} />
+        ))}
+		</div>
+    </Layout>
+	);
+};
+
+export default Hotel;
