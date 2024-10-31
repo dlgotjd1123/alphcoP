d3fbd3a7b2ca6ec977de8616b61bf51cdd33d81e
dlgotjd1123 <dlgotjd1123@naver.com>
2024-10-31 16:00:58 +0900

    메인 스크롤 효과

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,59 @@
-import Layout from '../components/Layout';
+import { useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import Layout from '../components/Layout';
+
 
 export default function Home() {
+	const mainImageRef = useRef(null);
+	const gridItemsRef = useRef([]);
 
+	useEffect(() => {
+		const observerOptions = {
+		threshold: 0.1, 
+		rootMargin: '0px 0px -100px 0px' 
+		};
+
+		const observer = new IntersectionObserver((entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+			entry.target.classList.add('visible'); 
+			} else {
+			entry.target.classList.remove('visible'); 
+			}
+		});
+		}, observerOptions);
+
+		gridItemsRef.current.forEach((item) => {
+		if (item) observer.observe(item);
+		});
+
+		return () => observer.disconnect();
+	}, []);
 
 	return (
 		<div className="main-page">
-			<div className="main-content">
-					<div className="main-image">
-					<Layout title='Home'>		
-						<p>Lorem ipsum dolor sit amet.</p>				
-					</Layout>
-			
+
+			<div className="main-image" ref={mainImageRef}>
+				<Layout title="Home">
+				<p>Lorem ipsum dolor sit amet.</p>
+				</Layout>
+			</div>
+
+
+			<div className="grid-section">
+				{['/hotel', '/notice', '/map'].map((path, index) => (
+				<div
+					className="grid-item"
+					key={index}
+					ref={(el) => (gridItemsRef.current[index] = el)}
+				>
+					<Link to={path}>
+					<img src={`/src/pages/img/img${index + 1}.jpg`} alt={`${index + 1}`} />
+					</Link>
+					<p>{index === 0 ? '객실' : index === 1 ? '공지사항' : '찾아오시는 길'}</p>
 				</div>
-				<div className="grid-section">
-					<div className="grid-item">
-						<Link to='/hotel'>
-							<img src="/src/pages/img/img1.jpg" alt=" 1" />
-						</Link>
-						<p>객실</p>
-					</div>
-					<div className="grid-item">
-						<Link to='/notice'>
-							<img src="/src/pages/img/img2.jpg" alt=" 2" />
-						</Link>
-						<p>공지사항</p>
-					</div>
-					<div className="grid-item">
-						<Link to='/map'>
-							<img src="/src/pages/img/img3.jpg" alt=" 3" />
-						</Link>
-						<p>찾아오시는 길</p>
-					</div>
-				</div>
+				))}
 			</div>
 		</div>
-		);
+	);
 	}
-	
